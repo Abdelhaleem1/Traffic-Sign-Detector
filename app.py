d36b9a3316189551d7a267362e5c905eb01b44f3
Abdelhalim <116656450+Abdelhaleem1@users.noreply.github.com>
2025-09-15 23:07:19 +0300

    initial commit

---
+++ app.py
@@ -0,0 +1,128 @@
+import streamlit as st
+import numpy as np
+import cv2
+from PIL import Image
+import easyocr
+from ultralytics import YOLO
+from main import picDetect
+import pathlib
+
+@st.cache_resource
+def load_model(weight_path="best.pt"):
+    return YOLO(weight_path)
+@st.cache_resource
+def load_reader():
+    return easyocr.Reader(['en'], gpu=False)
+
+model = load_model() 
+reader = load_reader()
+
+def load_css(file_path):
+    with open(file_path) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+css_path = pathlib.Path("style.css")
+load_css(css_path)
+
+
+st.set_page_config(page_title="Traffic Sign Detector", layout="wide", page_icon='🛑')
+st.title("Traffic Sign Detector 🛑")
+tabs = st.tabs(["📖 About", "🧪 Try", "📊 Results"])
+
+with tabs[0]:
+    st.header("About the Project")
+    st.write("""
+    This project focuses on detecting and classifying traffic signs such as 
+    **Speed Limits, Stop, No Parking, Pedestrian Crossings**, and more.  
+    It uses **YOLOv8 for object detection** and **OCR (EasyOCR)** for reading numbers inside speed limit signs.
+    """)
+
+    st.subheader("🛑 Supported Classes")
+
+    classes = [
+        "🟠 Bump"  ,
+        "⛔ No Parking "  ,
+        "🚫 No Stopping "  ,
+        "🔄 No U-Turn "  ,
+        "🚶 Pedestrian "  ,
+        "🚧 Road Work "  ,
+        "⚡ Speed Limit "  ,
+        "🛑 Stop"
+    ]
+    col1, col2 = st.columns(2)
+
+    with col1:
+        st.write(classes[0])
+        st.write(classes[1])
+        st.write(classes[2])
+        st.write(classes[3])
+
+    with col2:
+        st.write(classes[4])
+        st.write(classes[5]) 
+        st.write(classes[6])
+        st.write(classes[7])
+
+    st.subheader("👨‍💻 Team Members")
+    cols = st.columns(3)
+    with cols[0]:
+        st.markdown("**Abdelhalim Ahmed**  \n[LinkedIn](https://www.linkedin.com/in/abdelhalim-ahmed-720827248/)  |  [GitHub](https://github.com/Abdelhaleem1)")
+    with cols[1]:
+        st.markdown("**Mohannad Ashraf**  \n[LinkedIn](https://www.linkedin.com/in/mohannad-ashraf-888b24328/)  |  [GitHub](https://github.com/MohannadAshraf14)")
+    with cols[2]:
+        st.markdown("**Eyad Hazem**  \n[LinkedIn](https://www.linkedin.com/in/eyad-hazem-030574330)  |  [GitHub](https://github.com/Eyadhazem1)")
+
+    st.subheader("🛠️ Technologies Used")
+    st.write("""
+    - Python
+    - Roboflow
+    - Ultralytics YOLO
+    - OpenCV
+    - EasyOCR
+    - Streamlit""")
+
+with tabs[1]:
+    st.header("Try the Model")
+    demo_tab, upload_tab = st.tabs(["🎞️ Demo Samples", "📤 Upload Your Own"])
+    with demo_tab: 
+        st.subheader("Demo Images")
+        Demos = {
+            "Stop Sign": "demo/stop.jpg",
+            "Speed Limit": "demo/speed_limit.jpg", 
+            "No stopping": "demo/no_stopping.jpg",
+            "No Parking": "demo/no_parking.webp",
+        }
+
+        choice = st.selectbox("Choose a demo image:", list(Demos.keys()))
+        dempPath = Demos[choice]
+
+        with st.spinner("⏳ Processing image... Please wait."):
+            demoImg = picDetect(imgPath = dempPath, model=model, reader=reader)
+        demoImg = cv2.cvtColor(demoImg, cv2.COLOR_BGR2RGB)
+
+        col1, col2 = st.columns(2)
+        col1.image(dempPath, caption="Original", width='stretch')
+        col2.image(demoImg, caption="Detection", width='stretch')
+
+    with upload_tab:
+            st.subheader("Upload Your Own Image")
+            file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png", "webp"])
+            if file is not None:
+                img = np.array(Image.open(file))
+                img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+                with st.spinner("⏳ Processing image... Please wait."):
+                    output_img = picDetect(pic=img, model=model, reader=reader)
+                output_rgb = cv2.cvtColor(output_img, cv2.COLOR_BGR2RGB)
+                col1, col2 = st.columns(2)
+                col1.image(file, caption="Original Image", width='stretch')
+                col2.image(output_rgb, caption="Detected Image", width='stretch')
+
+with tabs[2]:
+    st.header("Model Results & Performance")
+    col1, col2, col3 = st.columns(3)
+    col1.metric("mAP50", "92%")
+    col2.metric("Precision", "93%")
+    col3.metric("Recall", "88%")
+    st.subheader("Detection Example")
+    col1, col2 = st.columns(2)
+    col1.image("demo/test4.webp", caption="Original Image", width='stretch')
+    col2.image(cv2.cvtColor(picDetect(imgPath="demo/test4.webp", model=model, reader=reader), cv2.COLOR_BGR2RGB), caption="Detected Image", width='stretch')
---
+++ main.py
@@ -0,0 +1,81 @@
+import cv2
+import warnings
+warnings.filterwarnings("ignore")
+
+def put_text(pic, x1, y1, x2, y2, color, text):
+    h, w, _ = pic.shape
+    fontSize = max(0.6, min(1, (y2-y1)/300))
+    textSize, baseLine = cv2.getTextSize(text, cv2.FONT_HERSHEY_SIMPLEX, fontSize, 1)
+    if y1 < textSize[1] + 10:
+        y1_text = y1 + textSize[1] + 5
+    else:
+        y1_text = y1 - 10
+    if ((x1 + textSize[0]) > w):
+        x1_text = x1 - ((x1 + textSize[0])-w)
+    else: x1_text = x1
+    cv2.rectangle(pic, (x1_text , y1_text - textSize[1] - baseLine), (x1_text + textSize[0], y1_text + baseLine), color, cv2.FILLED)
+    cv2.putText(pic,text,(x1_text, y1_text),cv2.FONT_HERSHEY_SIMPLEX,fontSize,(0, 0, 0), 1)
+    return pic
+
+
+def ocrDetect(img, x1, y1, x2, y2, reader):
+    crop = img[y1:y2, x1:x2]
+    crop = cv2.resize(crop, None, fx=3, fy=3, interpolation=cv2.INTER_CUBIC)
+    speed = reader.readtext(crop, detail=0, allowlist='0123456789')
+    if speed:
+        if speed[0] != "" :
+            if int(speed[0]) != 0:
+                return int(speed[0])
+    return ''
+
+def picDetect(imgPath=None, pic=None, model=None, reader = None):
+    if imgPath is not None:
+        pic = cv2.imread(imgPath)
+    h, w, _ = pic.shape
+    resizedpic = cv2.resize(pic, (640, 640))
+    box_colors = [(0, 255, 255),(128, 0, 128),(255, 0, 0),(0, 165, 255),
+                (0, 255, 0), (255, 0, 255), (255, 255, 0), (0, 0, 255)]
+    names = model.names
+    results = model.predict(resizedpic, conf = 0.45 , verbose=False)
+    result = results[0]
+    boxes = result.boxes
+    x_ratio = w / 640
+    y_ratio = h / 640
+    for box in boxes:
+        cls = box.cls.item()
+        clsName = names[cls]
+        conf = f"{int(box.conf.item()*100)}%"
+        x1, y1, x2, y2 = box.xyxy[0].cpu().numpy().astype(int)
+        x1, y1, x2, y2 = [int(x1 * x_ratio), int(y1 * y_ratio), int(x2 * x_ratio), int(y2 * y_ratio)]
+        color = box_colors[int(cls)]
+        cv2.rectangle(pic, (x1, y1), (x2, y2), color, 2)
+        if clsName == "Speed_Limit":
+            speed = ocrDetect(pic, x1, y1, x2, y2, reader)
+            text = f'{clsName} {speed}: {conf}'
+        else: 
+            text = f'{clsName}: {conf}'
+        pic = put_text(pic, x1, y1, x2, y2, color, text)
+    pic = cv2.resize(pic, (w, h))
+    return pic
+
+
+
+def vidDetect(vidPath, model=None, reader = None, outputPath=None):
+    vid = cv2.VideoCapture(vidPath)
+    fourcc = cv2.VideoWriter_fourcc(*"mp4v") 
+    fps = int(vid.get(cv2.CAP_PROP_FPS)) or 30
+    width = int(vid.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(vid.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    out = cv2.VideoWriter(outputPath,fourcc, fps, (width, height))
+    frame_count = 0
+    while True:
+        isFrame, frame = vid.read() # bool is there a pic, frame
+        if not isFrame:
+            break
+        frame_count += 1
+        if frame_count % 15 == 0:
+            out.write(frame)
+            continue
+        out.write(picDetect(pic=frame, model=model, reader=reader))
+    vid.release()
+    out.release()
